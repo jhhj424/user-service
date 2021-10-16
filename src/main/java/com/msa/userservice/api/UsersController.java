92afef1248d281e7f0e36789842ba195cfea8614
zito-s <jhhj424@naver.com>
2021-10-16 23:29:59 +0900

    feat: add welcome api

--- src/main/java/com/msa/userservice/api/UsersController.java
+++ src/main/java/com/msa/userservice/api/UsersController.java
@@ -1,15 +1,25 @@
 package com.msa.userservice.api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/")
 public class UsersController {
 
+    private final Environment env;
+
     @GetMapping("/health_check")
     public String status() {
         return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return env.getProperty("greeting.message");
     }
 }
