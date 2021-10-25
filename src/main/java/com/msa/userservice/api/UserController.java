88f1b73378ba05d0fd886bf4eb9dd980a97b48cb
jiho Shin <jhhj424@naver.com>
2021-10-25 21:47:51 +0900

    feat: UserController 추가 (creat)

---
+++ src/main/java/com/msa/userservice/api/UserController.java
@@ -0,0 +1,37 @@
+package com.msa.userservice.api;
+
+import com.msa.userservice.application.UserService;
+import com.msa.userservice.dto.UserDto;
+import com.msa.userservice.dto.UserRequest;
+import com.msa.userservice.dto.UserResponse;
+import com.msa.userservice.env.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private final Greeting greeting;
+    private final UserService userService;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest) {
+        UserDto user = userService.create(UserDto.of(userRequest));
+        return ResponseEntity.created(URI.create("/users/" + user.getUserId())).body(UserResponse.of(user));
+    }
+}
